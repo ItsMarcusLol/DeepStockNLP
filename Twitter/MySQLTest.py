8f1fd00918eeb353ab32add29b37ccc09c9c708a
Marcus Lee <leemg@plu.edu>
2020-11-22 17:30:00 -0800

    Now trying pymysql because previous did not work

--- Twitter/MySQLTest.py
+++ Twitter/MySQLTest.py
@@ -1,17 +1,11 @@
 #Used to test MySQL library and functions
 
-import MySQLdb
+import pymysql
 
-conn = MySQLdb.Connection(
-	host="localhost",
-	user="leemg",
-	passwd="Mahl2000",
-	db="CAP_stock2020"
-)
+conn = pymysql.connect("localhost","leemg","Mahl2000","CAP_stock2020")
 
-conn.query("""SELECT * FROM mytable""")
-result = conn.store_result()
-for i in range(result.num_rows()):
-    print(result.fetch_row())
+cursor = db.cursor()
+
+print("connected")
 
 conn.close()
