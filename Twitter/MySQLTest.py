683f60c3dcd4883ccfdbba3e494e6430eb791088
Marcus Lee <leemg@plu.edu>
2020-11-24 15:05:29 -0800

    changed module to pymysql

--- Twitter/MySQLTest.py
+++ Twitter/MySQLTest.py
@@ -1,10 +1,22 @@
 #Used to test MySQL library and functions
 
-from MySQLdb import _mysql
+import pymysql
 
 #conn = _mysql.connect(host='localhost',user='leemg',passwd='MarLee21!',db='CAP_stock2020')
-conn = _mysql.connect(host='127.0.0.1',user='leemg',passwd='MarLee21!',db='CAP_stock2020',ssl_disabled='True')
+#conn = _mysql.connect(host='127.0.0.1',user='leemg',passwd='MarLee21!',db='CAP_stock2020',ssl_disabled='True')
 
-print(conn)
+conn = pymysql.connect('localhost', 'leemg', 'MarLee21!', 'CAP_stock2020')
+
+cursor = db.cursor()
+
+query = "SELECT * FROM GoogleStockTweets"
+
+cursor.execute(query)
+results = cursor.fetchall()
+
+for row in results:
+	print(row)
+
+#print(conn)
 
 conn.close()
