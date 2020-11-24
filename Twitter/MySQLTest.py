b5ba20a8e8e3f170c664d6ee1ce25b1cf3b1ce0b
Marcus Lee <leemg@plu.edu>
2020-11-24 14:32:04 -0800

    changes localhost connection

--- Twitter/MySQLTest.py
+++ Twitter/MySQLTest.py
@@ -2,7 +2,7 @@
 
 from MySQLdb import _mysql
 
-conn = _mysql.connect("localhost","leemg","MarLee21!","CAP_stock2020")
+conn = _mysql.connect("127.0.0.1","leemg","MarLee21!","CAP_stock2020")
 
 print(conn)
 
