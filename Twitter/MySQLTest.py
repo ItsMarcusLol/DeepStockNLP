971f0f983f256f826a1767c53528bfdc5dd46bc3
Marcus Lee <leemg@plu.edu>
2020-11-24 14:44:03 -0800

    set use_pure to true'

--- Twitter/MySQLTest.py
+++ Twitter/MySQLTest.py
@@ -2,7 +2,8 @@
 
 from MySQLdb import _mysql
 
-conn = _mysql.connect("127.0.0.1","leemg","MarLee21!","CAP_stock2020")
+#conn = _mysql.connect(host='localhost',user='leemg',passwd='MarLee21!',db='CAP_stock2020')
+conn = _mysql.connect(host='127.0.0.1',user='leemg',passwd='MarLee21!',db='CAP_stock2020',use_pure=True)
 
 print(conn)
 
