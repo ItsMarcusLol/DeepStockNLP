34e0310794c4d217ee60da96c5da65de075e7742
Marcus Lee <leemg@plu.edu>
2020-11-24 14:51:41 -0800

    set ssl_disabled to true

--- Twitter/MySQLTest.py
+++ Twitter/MySQLTest.py
@@ -3,7 +3,7 @@
 from MySQLdb import _mysql
 
 #conn = _mysql.connect(host='localhost',user='leemg',passwd='MarLee21!',db='CAP_stock2020')
-conn = _mysql.connect(host='127.0.0.1',user='leemg',passwd='MarLee21!',db='CAP_stock2020',use_pure=True)
+conn = _mysql.connect(host='127.0.0.1',user='leemg',passwd='MarLee21!',db='CAP_stock2020',ssl_disabled='True')
 
 print(conn)
 
