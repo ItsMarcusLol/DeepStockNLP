fd1170115de120fdb5b42fdabcf0922fa0e6f04a
Marcus Lee <leemg@plu.edu>
2020-11-26 12:12:20 -0800

    Changed query string

--- Twitter/InsertStock.py
+++ Twitter/InsertStock.py
@@ -27,7 +27,7 @@ print(time)
 tweet_data = ['tesla', 'Ant', 359, 456, time, 'None', 0, 0, 0, 0, 'Told my mom how I made $800+ just from Tesla and stocks in the past week using Robinhood and she said it was a scam']
 
 query = "INSERT INTO %s(username,followers,following,date_tweeted,retweet_author,retweet_followers,retweet_following,retweets,favorites,status) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-args = (str(stock_table_name),tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4]),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10])
+args = (stock_table_name.replace("\'",""),tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4]),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10])
 cursor.execute(query, args)
 conn.commit()
 print(cursor._last_executed)
