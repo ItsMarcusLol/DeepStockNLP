8ba3c9d1aad30580ead4d6e5fbbfdf96e6fd25b6
Marcus Lee <leemg@plu.edu>
2020-11-25 17:45:30 -0800

    refactored variable names

--- Twitter/GetTwitterData.py
+++ Twitter/GetTwitterData.py
@@ -81,13 +81,13 @@ def processThread(in_q):
 			query = "INSERT INTO %s(username,followers,following,date_tweeted,retweet_author,retweet_followers,retweet_following,retweets,favorites,status) " \
 				"VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 			args = (stock_table_name,tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4]),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10],)
-			insertCursor.execute(query, args)
+			cursor.execute(query, args)
 			conn.commit()
 		except:
 			conn.rollback()
 			conn.close()
 			cursor.close()
-			os.exit(1)
+			sys.exit(1)
 
 def getStockNames():
 	cursor = conn.cursor()
