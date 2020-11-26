d82e32b3bb54eb6808a511d433228e9748980288
Marcus Lee <leemg@plu.edu>
2020-11-25 18:06:15 -0800

    Testing what query looks like

--- Twitter/Tests/GetListOfStocksTest.py
+++ Twitter/Tests/GetListOfStocksTest.py
@@ -23,7 +23,7 @@ finally:
 	for stock in stock_tables:
 		stock_table_name = stock_tables[stock]
 		query = "INSERT INTO %s(username,followers,following,date_tweeted,retweet_author,retweet_followers,retweet_following,retweets,favorites,status) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-		args = (stock_table_name,tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4]),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10],)
+		args = (stock_table_name,"username",1,1,"date","retweet_author",1,1,1,1,"status",)
 		print(query, args)	
 	conn.close()
 	cursor.close()
