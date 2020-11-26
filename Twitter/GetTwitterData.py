0090bcfb5370028c39e33e83c7f455e0f7afde09
Marcus Lee <leemg@plu.edu>
2020-11-26 13:10:29 -0800

    added logic to not add a record to the database if the tweet is already stored

--- Twitter/GetTwitterData.py
+++ Twitter/GetTwitterData.py
@@ -71,8 +71,17 @@ def searchTweets(out_q, word, langauge):
 			exit()
 			break
 
-def TweetAlreadySeen(tweet_data, cursor):
-	return
+def tweetAlreadySeen(tweet_data, cursor):
+	stock_table_name = stock_tables[tweet_data[0]]
+	query = "SELECT * FROM "+stock_table_name+" WHERE username=%s AND followers=%s AND following=%s AND date_tweeted=%s AND retweet_author=%s AND retweet_followers=%s AND retweet_following=%s AND retweets=%s AND favorites=%s AND status=%s"
+	args = (tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4].replace(hour=0,minute=0, second=0)),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10])
+	cursor.execute(query, args)
+	result = cursor.fetchone()
+	row_count = cursor.rowcount
+	if row_count == 1:
+		return True
+	else:
+		return False
 
 def processThread(in_q):
 	cursor = conn.cursor()
@@ -80,6 +89,8 @@ def processThread(in_q):
 	while(True):
 		tweet_data = in_q.get()
 		print(tweet_data)
+		if tweetAlreadySeen(tweetAlreadySeen(tweet_data, cursor)):
+			continue
 		stock_name = tweet_data[0]
 		stock_table_name = stock_tables[stock_name]
 		try: 
--- Twitter/Tests/Test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-
-time = datetime.datetime(2020, 11, 26, 19, 29, 13)
-print(time)
-print(time.replace(hour=0,minute=0, second=0))
-time = datetime.datetime(2020, 11, 26, 0, 0, 0)
-print(time)
