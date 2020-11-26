136d200313420f09ce8a1d86c7cda521c5e6a73f
Marcus Lee <leemg@plu.edu>
2020-11-26 12:09:44 -0800

    Changed query string

--- Twitter/GetTwitterData.py
+++ Twitter/GetTwitterData.py
@@ -81,7 +81,7 @@ def processThread(in_q):
 		stock_table_name = stock_tables[stock_name]
 		try: 
 			query = "INSERT INTO %s(username,followers,following,date_tweeted,retweet_author,retweet_followers,retweet_following,retweets,favorites,status) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-			args = (stock_table_name,tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4]),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10])
+			args = (str(stock_table_name),tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4]),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10])
 			cursor.execute(query, args)
 			conn.commit()
 			print(cursor._last_executed)
