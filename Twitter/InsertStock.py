1224689df15c1f05c6a7daae6d934e59da082286
Marcus Lee <leemg@plu.edu>
2020-11-26 12:05:07 -0800

    Testing adding a tweet to the database

--- Twitter/InsertStock.py
+++ Twitter/InsertStock.py
@@ -1,5 +1,6 @@
 import pymysql
 from datetime import datetime
+import time
 
 
 conn = pymysql.connect('localhost', 'leemg', 'MarLee21!', 'CAP_stock2020')
@@ -21,8 +22,9 @@ except:
 	print ("Error: unable to fetch data")
 
 stock_table_name = stock_tables['tesla']
-
-tweet_data = ['tesla', 'Ant', 359, 456, datetime.datetime(2020, 11, 26, 19, 29, 13), 'None', 0, 0, 0, 0, 'Told my mom how I made $800+ just from Tesla and stocks in the past week using Robinhood and she said it was a scam']
+time = datetime.datetime(2020, 11, 26, 19, 29, 13)
+print(time)
+tweet_data = ['tesla', 'Ant', 359, 456, time, 'None', 0, 0, 0, 0, 'Told my mom how I made $800+ just from Tesla and stocks in the past week using Robinhood and she said it was a scam']
 
 query = "INSERT INTO %s(username,followers,following,date_tweeted,retweet_author,retweet_followers,retweet_following,retweets,favorites,status) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 args = (stock_table_name,tweet_data[1],tweet_data[2],tweet_data[3],str(tweet_data[4]),tweet_data[5],tweet_data[6],tweet_data[7],tweet_data[8],tweet_data[9],tweet_data[10])
