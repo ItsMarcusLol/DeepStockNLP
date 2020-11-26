dd22ae5a4dd80ecfa0102e578e47243f8d78059d
Marcus Lee <leemg@plu.edu>
2020-11-25 17:15:15 -0800

    Fixed more indentation

--- Twitter/Tests/GetListOfStocksTest.py
+++ Twitter/Tests/GetListOfStocksTest.py
@@ -9,14 +9,13 @@ cursor = conn.cursor()
 query = "SELECT * FROM ListOfStocks"
 
 try:
-   cursor.execute(query)
-   results = cursor.fetchall()
-
-   for row in results:
-      stock = row[0]
-      table_name = row[1]
-      stock_tables[stock] = table_name
-      print(stock_tables)
+	cursor.execute(query)
+	results = cursor.fetchall()
+	for row in results:
+		stock = row[0]
+		table_name = row[1]
+		stock_tables[stock] = table_name
+	print(stock_tables)
 except:
    print ("Error: unable to fetch data")
 finally:
