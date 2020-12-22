8bd328a0fc508102378aad77f6918dd7dfcc0948
Marcus Lee <leemg@plu.edu>
2020-12-22 14:46:34 -0800

    changed to pass unique cursor into each thread

--- Twitter/dbManagement/TweetsToCSV.py
+++ Twitter/dbManagement/TweetsToCSV.py
@@ -4,23 +4,20 @@ from threading import Thread
 from queue import Queue
 
 
-conn = pymysql.connect('localhost', 'leemg', 'MarLee21!', 'CAP_stock2020')
 
-def tweetsToCsv(table_name):
+def tweetsToCsv(table_name, cursor, conn):
 	print(table_name)
 	#filename = table_name+".csv"
 	#file = open(filename, "w")
 	#file.write("Woops! I have deleted the content!")
 	#file.close()
 	query = "SELECT * FROM " + table_name
-	cur = conn.cursor()
-	cur.execute(query)
-	results = cur.fetchall()
+	cursor.execute(query)
+	results = cursor.fetchall()
 	for row in results:
-		line = ""
-		for element in row:
-			line = line + " " + str(element)
-	cur.close()
+		print(row)
+	cursor.close()
+	conn.close()
 	print("Done")
 
 def createThreads():
@@ -34,10 +31,15 @@ def createThreads():
 		stock = row[0]
 		table_name = row[1]
 		stock_tables[stock] = table_name
-	cursor.close()
+
 	for stock in stock_tables:
-		table_name = stock_tables[stock]
-		thread = Thread(target=tweetsToCsv, args=(table_name, ))
-		thread.start()
+    	conn = pymysql.connect('localhost', 'leemg', 'MarLee21!', 'CAP_stock2020')
+    	cursor = conn.cursor()
+    	table_name = stock_tables[stock]
+    	thread = Thread(target=tweetsToCsv, args=(table_name, cursor, conn, ))
+    	for th in threads:
+    		th.start()
+		for t in threads:
+    		t.join()
 
 createThreads()
