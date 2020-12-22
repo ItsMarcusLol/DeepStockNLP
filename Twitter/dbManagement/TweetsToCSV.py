9a0586320fa95b9c0c2f81327adad0eba0089c99
Marcus Lee <leemg@plu.edu>
2020-12-22 14:05:15 -0800

    Added call function line

--- Twitter/dbManagement/TweetsToCSV.py
+++ Twitter/dbManagement/TweetsToCSV.py
@@ -13,33 +13,28 @@ def tweetsToCsv(table_name):
 	#file.close()
 	query = "SELECT * FROM " + table_name
 	cursor = conn.cursor()
-	try:
-		cursor.execute(query)
-		results = cursor.fetchall()
-		for row in results:
-			print(row)
-	except:
-		print(sys.exc_info()[0])
-		print("ERROR")
+	cursor.execute(query)
+	results = cursor.fetchall()
+	for row in results:
+		print(row)
+	print(sys.exc_info()[0])
+	print("ERROR")
 
 def createThreads():
 	q = Queue()
 	query = "SELECT * FROM List_Of_Stocks"
 	cursor = conn.cursor()
 	stock_tables = {}
-	try:
-		cursor.execute(query)
-		results = cursor.fetchall()
-		for row in results:
-			stock = row[0]
-			table_name = row[1]
-			stock_tables[stock] = table_name
-	except:
-   		print ("Error: unable to fetch data")
-   		sys.exit(1)
-	finally:
-		cursor.close()
-		for stock in stock_tables:
-			thread = Thread(target=tweetsToCsv, args=(stock_tables[stock_tables]), )
-			thread.start()
-		print("Threads Started...")
+	cursor.execute(query)
+	results = cursor.fetchall()
+	for row in results:
+		stock = row[0]
+		table_name = row[1]
+		stock_tables[stock] = table_name
+	cursor.close()
+	for stock in stock_tables:
+		thread = Thread(target=tweetsToCsv, args=(stock_tables[stock_tables]), )
+		thread.start()
+	print("Threads Started...")
+
+createThreads()
