b603decf50fecf04828a9564545a696cc15af6bf
Marcus Lee <leemg@plu.edu>
2021-01-02 12:59:47 -0800

    Setup for forum API and docker files. Testing get twitter tweets

--- website/src/resources/ForumResource.py
+++ website/src/resources/ForumResource.py
@@ -1,13 +1,8 @@
-# Account Data Resource
 from flask import Flask
 from flask_restful import Api
-from account.AccountController import Login, Account
 
 app = Flask(__name__)
 api = Api(app)
-
-api.add_resource(Login, '/login')
-api.add_resource(Account, '/account')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
