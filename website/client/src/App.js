56ab5e26d3316ca9c164fab43194dc9069dca97d
Erika <powellea@plu.edu>
2021-02-08 17:30:49 -0800

    Added more components, added a grid system for components placement

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -7,6 +7,14 @@ import Login from './components/pages/Login';
 import SignUp from './components/pages/SignUp';
 import FetchStockPrices from './components/layout/FetchStockPrices';
 import MaterialTable from './components/layout/MaterialTable';
+import DayPriceGraph from './components/layout/DayPriceGraph';
+import HTable from './components/layout/HTable';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+
 
 class App extends Component {
   state = {
@@ -16,9 +24,12 @@ class App extends Component {
     authenticated: false
   }
 
+
   login = (username, password) => {
     this.setState({ username: username, password: password, userId: 123, authenticated: true });
   }
+
+
 
   render() {
     console.log(this.state);
@@ -26,11 +37,38 @@ class App extends Component {
       <Router>
         <div className="App">
           <div className="container">
+          {/* <Grid container spacing={2}>
+          <Grid item xs={12}> */}
+            {/* <Paper >xs=12</Paper> */}
             <Header />
+          {/* </Grid> */}
             <Route exact path="/" render={props => (
               <React.Fragment>
-                <FetchStockPrices />
-                <MaterialTable /> 
+                <Grid container spacing={2}>
+                <Grid item xs={12}>
+                   {/* <Paper >xs=12</Paper> */}
+                   <FetchStockPrices />
+                </Grid>
+                <Grid item xs={3}>
+                   {/* <Paper >xs=3</Paper> */}
+                   <DayPriceGraph />
+                </Grid>
+                <Grid item xs={3.2}>
+                   {/* <Paper >xs=3</Paper> */}
+                   <DayPriceGraph />
+                </Grid>
+                
+                <Grid item xs={6}>
+                   {/* <Paper >xs=6</Paper> */}
+                   <MaterialTable /> 
+                </Grid>
+
+                <Grid item xs={6}>
+                   {/* <Paper >xs=6</Paper> */}
+                   <HTable /> 
+                </Grid>
+               </Grid>
+                
               </React.Fragment>
             )} />
             <Route path="/about" component={About} />
