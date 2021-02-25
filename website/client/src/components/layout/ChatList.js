29c91196002269edf32efdef09fd1ec8ec6468e1
Erika <powellea@plu.edu>
2021-02-24 20:22:59 -0800

    changed the list items, so can take more than one post for a user. Also changed the style of the list

--- website/client/src/components/layout/ChatList.js
+++ website/client/src/components/layout/ChatList.js
@@ -4,35 +4,52 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import Divider from '@material-ui/core/Divider';
+import { shadows } from '@material-ui/system';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: 360,
+    maxWidth: 500,
     backgroundColor: theme.palette.background.paper,
     position: 'relative',
     overflow: 'auto',
     maxHeight: 300,
+    
   },
   listSection: {
     backgroundColor: 'inherit',
+    fontSize: 60,
+    
+  },
+  listSubH: {
+    backgroundColor: 'inherit',
+    
   },
   ul: {
     backgroundColor: 'inherit',
     padding: 0,
+    
   },
+  listSubHeaderRoot: {
+    backgroundColor: 'lightgray',
+    color: '#252525',
+    fontSize: 18,
+    fontFamily: 'Arial'
+        /* To change the font, use the fontFamily rule */
+  }
 }));
 
 
 let acctPts = new Map()
 acctPts.set('Jessie123', {stock: "Google", post:[ "I think Google stock will go up", "Def will go up", "High key will go up"]})
-acctPts.set('Hilary321', {stock: "Google", post: "I think Google stock will go down"})
-acctPts.set('GGG', {stock: "Google", post: "I think Google stock will go up"})
-acctPts.set('AAA', {stock: "Google", post: "I think Google stock will go down"})
-acctPts.set('Gund', {stock: "Google", post: "I think Google stock will go up"})
-acctPts.set('jodml', {stock: "Google", post: "I think Google stock will go down"})
-acctPts.set('pearbear', {stock: "Google", post: "I think Google stock will go up"})
-acctPts.set('jimfod', {stock: "Google", post: "I think Google stock will go down"})
+acctPts.set('Hilary321', {stock: "Google", post: ["I think Google stock will go down", "I love Google stock"]})
+acctPts.set('GGG', {stock: "Google", post: ["I think Google stock will go up", " I make so much mmoney"]})
+acctPts.set('AAA', {stock: "Google", post: ["I think Google stock will go down"]})
+acctPts.set('Gund', {stock: "Google", post: ["I think Google stock will go up"]})
+acctPts.set('jodml', {stock: "Google", post:[ "I think Google stock will go down"]})
+acctPts.set('pearbear', {stock: "Google", post: ["I think Google stock will go up"]})
+acctPts.set('jimfod', {stock: "Google", post: ["I think Google stock will go down"]})
 // acctPts..get('Jessie') // {phone: "213-555-1234", address: "123 N 1st Ave"}
 // acctPts..delete('Raymond') // false
 
@@ -58,13 +75,32 @@ export default function PinnedSubheaderList() {
          {keys.map((sectionId) => (
         <li key={`section-${sectionId}`} className={classes.listSection}>
           <ul className={classes.ul}>
-            <ListSubheader>{` ${sectionId}`}</ListSubheader>
+            <ListSubheader classes={{root:classes.listSubHeaderRoot}}>{` ${sectionId}`}</ListSubheader >
 
             {/* {keys.map((item) => ( */}
 
-              <ListItem key={`item-${sectionId}-`}>
+
+              {(acctPts.get(sectionId).post).map((p) => (
+                 
+                <ListItem key={`item-${sectionId}-`}>
+                <ListItemText primary={` ${acctPts.get(sectionId).stock}: ${p}`} />
+              
+                
+              </ListItem> 
+
+             )
+             
+             )
+             
+             }
+             <Divider />
+
+              
+
+
+              {/* <ListItem key={`item-${sectionId}-`}>
                 <ListItemText primary={` ${acctPts.get(sectionId).stock}: ${acctPts.get(sectionId).post}`} />
-              </ListItem>
+              </ListItem> */}
 
             {/* ))} */}
           </ul>
