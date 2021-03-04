474a13c0dd908aedb016c8cb0118c5e5b0fe710f
Erika <powellea@plu.edu>
2021-03-03 21:49:45 -0800

    Added a drop down menu for which stock post is about. Also added grid and styles.

--- website/client/src/components/layout/Chat.js
+++ website/client/src/components/layout/Chat.js
@@ -1,83 +1,159 @@
-import React from 'react';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import Icon from '@material-ui/icons/Send';
-// import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
-// import Icon from '@material-ui/core/Icon';
-import SaveIcon from '@material-ui/icons/Save';
-// import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import ChatList from './ChatList';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import TextField from '@material-ui/core/TextField';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
+
+
+
+import Select from '@material-ui/core/Select';
+import Grid from '@material-ui/core/Grid';
+import ChatList from './ChatList';
+
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Icon from '@material-ui/icons/Send';
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import { withStyles } from '@material-ui/core/styles';
+
+
+
+const styles = (theme) => ({
+  // fab: {
+  //   position: 'fixed',
+  //   bottom: theme.spacing(2),
+  //   right: theme.spacing(2),
+  // },
+  formControl: {
+    margin: theme.spacing(0),
+    minWidth: 90,
+    
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(0),
+  },
+  textF: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(0),
+      width: '26ch',
+      // right: theme.spacing(2)
     },
   },
-}));
-
-// export default function Chat() {
-  export default class PriceTable extends React.Component {
-
-
-    state = {
-      loading: true, 
-      prices: null,
-  };
-
-  async componentDidMount() {
-    
-      const data = "'hiS"
+  button: {
+    '& > *': {
       
+    width:125,
+    height: 40,
+    marginLeft:20,
+    marginRight:25,
+    // marginDown: 5,
+    // margin: 30,
+ 
+      margin: theme.spacing(4),
+      
+      // left: theme.spacing(2)
+    },
+  },
+});
 
-    console.log(data)
-      this.setState({ posts:data, loading: false});
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2,
+      stock: '',
+      searchNodes: ""
+
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
-  // classes = useStyles();
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    console.log(value)
+    console.log(name)
+
+    this.setState({
+      [name]: value
+    });
+  }
 
   render() {
-    // if (this.state.loading){
-    //     return <LoadingSymbol />
-    // }
-
-  return (
-    <div>
-          <form  noValidate autoComplete="off">
-          {/* <form className={classes.root} noValidate autoComplete="off"> */}
-       {/* <List className={classes.root}> */}
+    const {classes} = this.props;
+    console.log(this.props)
+    return (
       
-      <ChatList/>
-    
-    
-   
-      <TextField id="outlined-basic" label="Outlined"  multiline rows = {4} variant="outlined" />
-      <Button
+       
+       <form>
+ <Grid container spacing={2}>
+           {/* <Grid item xs = {12}></Grid> */}
+      <Grid item xs = {12}>
+        <ChatList/>
+        </Grid>
+        <Grid item xs = {2}>
+        <FormControl variant="outlined" className={classes.formControl}>
+        {/* <FormControl variant="outlined" > */}
+        <InputLabel id="demo-simple-select-outlined-label">Stock</InputLabel>
+        <Select
+          name="stock"
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={this.state.stock}
+          onChange={this.handleInputChange}
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={'GOOGL'}>Google </MenuItem>
+          <MenuItem value={'AMZN'}>Amazon</MenuItem>
+          <MenuItem value={'F'}>Ford</MenuItem>
+          <MenuItem value={'WMT'}>Walmart</MenuItem>
+        </Select>
+        </FormControl>
+        </Grid>
+
+
+        <Grid item xs = {4}>
+        {/* <form className={classes.textF} noValidate autoComplete="off"> */}
+        <TextField id="outlined-basic" label="Outlined"  multiline rows = {4} variant="outlined" />
+
+        {/* </form> */}
+        </Grid>
+
+       
+        <Grid item xs = {3}>
+          <form className={classes.button} noValidate autoComplete="off">
+        <Button
+        className={classes.button}
+        
           variant="contained"
           color="primary"
         //   className={classes.button}
           endIcon={<Icon>send</Icon>}
-          
-          
-        >
-          Send
+           >
+          Post
         </Button>
-    </form>
-    </div>
-  );
-}
+        </form>
+        </Grid>
+     
+        </Grid>
+       {/* </Grid> */}
+
+       </form>
+       
+  
+      
+
+    );
   }
+}
+
+export default withStyles(styles, )(Chat);
