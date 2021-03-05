8210475135a6c56fe8475c01b28f4cb10443db4b
Erika <powellea@plu.edu>
2021-03-04 22:57:45 -0800

    Added Grid to chat so that the input below can be aligned... Still need to be aligned better, right now they are centered.

--- website/client/src/components/layout/Chat.js
+++ website/client/src/components/layout/Chat.js
@@ -5,27 +5,22 @@
 import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
 import ChatList from './ChatList';
-
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Icon from '@material-ui/icons/Send';
-
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import { withStyles } from '@material-ui/core/styles';
 
 
 
 const styles = (theme) => ({
-  // fab: {
-  //   position: 'fixed',
-  //   bottom: theme.spacing(2),
-  //   right: theme.spacing(2),
-  // },
+  fC:{
+    width: 1000,
+
+  },
   formControl: {
     margin: theme.spacing(0),
     minWidth: 90,
@@ -37,8 +32,7 @@ const styles = (theme) => ({
   textF: {
     '& .MuiTextField-root': {
       margin: theme.spacing(0),
-      width: '26ch',
-      // right: theme.spacing(2)
+      width: '30ch',
     },
   },
   button: {
@@ -48,12 +42,7 @@ const styles = (theme) => ({
     height: 40,
     marginLeft:20,
     marginRight:25,
-    // marginDown: 5,
-    // margin: 30,
- 
-      margin: theme.spacing(4),
-      
-      // left: theme.spacing(2)
+    margin: theme.spacing(4),
     },
   },
 });
@@ -66,7 +55,8 @@ class Chat extends React.Component {
       isGoing: true,
       numberOfGuests: 2,
       stock: '',
-      searchNodes: ""
+      post: false,
+      // searchNodes: ""
 
     };
 
@@ -77,6 +67,7 @@ class Chat extends React.Component {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+    
     console.log(value)
     console.log(name)
 
@@ -87,71 +78,85 @@ class Chat extends React.Component {
 
   render() {
     const {classes} = this.props;
-    console.log(this.props)
-    return (
-      
-       
-       <form>
- <Grid container spacing={2}>
-           {/* <Grid item xs = {12}></Grid> */}
-      <Grid item xs = {12}>
-        <ChatList/>
+    
+    return (  
+       <div>
+    
+      <Grid container spacing={2} container
+  direction="row"
+  justify="space-evenly"
+  alignItems="center">
+        <Grid item xs = {12}
+         container
+         direction="row"
+         justify="center"
+         alignItems="center">
+          <ChatList/>
         </Grid>
-        <Grid item xs = {2}>
-        <FormControl variant="outlined" className={classes.formControl}>
-        {/* <FormControl variant="outlined" > */}
-        <InputLabel id="demo-simple-select-outlined-label">Stock</InputLabel>
-        <Select
-          name="stock"
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={this.state.stock}
-          onChange={this.handleInputChange}
-          label="Age"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={'GOOGL'}>Google </MenuItem>
-          <MenuItem value={'AMZN'}>Amazon</MenuItem>
-          <MenuItem value={'F'}>Ford</MenuItem>
-          <MenuItem value={'WMT'}>Walmart</MenuItem>
-        </Select>
-        </FormControl>
-        </Grid>
-
-
-        <Grid item xs = {4}>
-        {/* <form className={classes.textF} noValidate autoComplete="off"> */}
-        <TextField id="outlined-basic" label="Outlined"  multiline rows = {4} variant="outlined" />
-
-        {/* </form> */}
-        </Grid>
-
-       
-        <Grid item xs = {3}>
-          <form className={classes.button} noValidate autoComplete="off">
-        <Button
-        className={classes.button}
         
-          variant="contained"
-          color="primary"
-        //   className={classes.button}
-          endIcon={<Icon>send</Icon>}
-           >
-          Post
-        </Button>
-        </form>
-        </Grid>
-     
-        </Grid>
-       {/* </Grid> */}
 
-       </form>
-       
-  
-      
+          <Grid item xs = {4}
+           container
+           direction="row"
+           justify="center"
+           alignItems="center">
+            <form className={classes.textF} noValidate autoComplete="off">
+              <TextField id="outlined-basic" label="Outlined"  multiline rows = {5} variant="outlined"/>
+            </form>
+          </Grid>
 
+        
+          <Grid item xs = {2}
+         container
+         direction="row"
+         justify="center"
+         alignItems="center">
+            <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">Stock</InputLabel>
+              <Select
+                name="stock"
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value={this.state.stock}
+                onChange={this.handleInputChange}
+                label="Age"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={'GOOGL'}>Google </MenuItem>
+                <MenuItem value={'AMZN'}>Amazon</MenuItem>
+                <MenuItem value={'F'}>Ford</MenuItem>
+                <MenuItem value={'WMT'}>Walmart</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+
+          <Grid item xs = {3} 
+          container
+          direction="row"
+          justify="center"
+          alignItems="center">
+            {/* <form className={classes.button} noValidate autoComplete="off"> */}
+              <Button
+              name = "post"
+              classes = {classes.button}
+               onClick= {
+                 this.handleInputChange
+
+                }
+                variant="contained"
+                color="primary"
+                endIcon={<Icon>send</Icon>}
+                >
+                Post
+              </Button>
+          {/* </form> */}
+          </Grid> 
+        </Grid>
+ 
+        </div>
     );
   }
 }
