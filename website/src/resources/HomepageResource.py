e873e3d74c2e027de5520b5994fd41f1dd209a46
Marcus Lee <leemg@plu.edu>
2021-03-12 10:58:14 -0800

    changed file structure of homepage and forum service

--- website/src/resources/HomepageResource.py
+++ website/src/resources/HomepageResource.py
@@ -1,9 +1,15 @@
-from flask import Flask
-from flask_restful import Api
-from homepage.HomepageController import HomepageController
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from homepage.HomepageManager import HomepageManager
 
 app = Flask(__name__)
 api = Api(app)
+
+homepageManager = HomepageManager()
+
+class HomepageController(Resource):
+    def get(self):
+        return homepageManager.get_homepage()
 
 api.add_resource(HomepageController, '/homepage')
 
