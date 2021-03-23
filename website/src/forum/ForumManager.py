17215c9c2ee775a52ff1e8c70f2f9c37bcc0e903
Marcus Lee <leemg@plu.edu>
2021-03-23 15:39:52 -0700

    Added implementation for forum service in resource and the manager

--- website/src/forum/ForumManager.py
+++ website/src/forum/ForumManager.py
@@ -1,10 +1,59 @@
+from random import randint
+import mysql.connector
+import datetime
+
+conn = mysql.connector.connect(user='root', password='MarLee21!', host='db', database='accounts')
+
 class ForumManager():
     def getConversation(self, id):
-        return {
-            'conversation': [
-                'marcus',
-                '1/1/1951',
-                123456,
-                id
-            ]        
-        }
+        return None
+    
+    # TODO check to see if user_id is valid and if message length is adequate
+    def create_conversation(self, user_id, message):
+        id = self.create_conversation_id()
+        cursor = conn.cursor() 
+        utc = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        query = "INSERT INTO forum_data(conversation_id, user_id, date_time, count, text) VALUES(" + str(id) +","+str(user_id)+","+str(utc)+","+str(0)+",\"" + message +"\")"
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        return True
+
+    def create_conversation_id(self):
+        cursor = conn.cursor()
+        while(True):
+            id = randint(1, 2147483647)
+            query = "SELECT * FROM forum_data WHERE conversation_id=" + str(id)
+            cursor.execute(query)
+            msg = cursor.fetchone()
+            if not msg:
+                cursor.close()
+                return id
+
+class MessageManager():
+    def getMessage(self, id, user_id, count):
+        cursor = conn.cursor()
+        query = "SELECT * FROM forum_data WHERE conversation_id=" + str(id)+  " AND user_id=" + str(user_id) + " AND count=" + str(count)
+        cursor.execute(query)
+        msg = cursor.fetchone()
+        cursor.close()
+        if (not msg):
+            return None
+        else:
+            return msg
+
+    # TODO check to see if user_id is valid and if message length is adequate
+    def create_messsage(self, conversation_id, user_id, message):
+        cursor = conn.cursor()
+        query = "SELECT COUNT(*) FROM forum_data WHERE conversation_id=" + str(conversation_id)
+        rows = cursor.execute(query)
+        if (int(rows) == 0):
+            cursor.close()
+            return False
+        else:
+            utc = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+            query = "INSERT INTO forum_data(conversation_id, user_id, date_time, count, text) VALUES(" + str(conversation_id) +","+str(user_id)+","+str(utc)+","+str(rows)+",\"" + message +"\")"
+            cursor.execute(query)
+            conn.commit()
+            cursor.close()
+            return True
