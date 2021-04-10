7385497d4beda8ab84fd97cc2018cd4825b40125
Marcus Lee <leemg@plu.edu>
2021-04-09 23:33:13 -0700

    Sleeping to wait on headline data

--- website/src/homepage/HomepageManager.py
+++ website/src/homepage/HomepageManager.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, make_response
 from urllib.request import urlopen
 import json
+import time
 
 class HomepageManager():
     def get_headlines(self, symbol):
         key = "f0448bd30a7028e245052fcf3caa0837"
         url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=" + symbol + "&limit=4&apikey=" + key
         response = urlopen(url)
+        time.sleep(4)
         data = response.read().decode("utf-8")
         headlines = json.loads(data)
         return make_response(jsonify(headlines), 200)
