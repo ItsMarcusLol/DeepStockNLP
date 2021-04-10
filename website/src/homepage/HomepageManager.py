e00c0af22ed413cd58e3324f7c8c0e447ebc97d8
Marcus Lee <leemg@plu.edu>
2021-04-09 23:41:51 -0700

    Sleeping to wait on headline data

--- website/src/homepage/HomepageManager.py
+++ website/src/homepage/HomepageManager.py
@@ -8,7 +8,6 @@ class HomepageManager():
         key = "f0448bd30a7028e245052fcf3caa0837"
         url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=" + symbol + "&limit=4&apikey=" + key
         response = urlopen(url)
-        time.sleep(4)
         data = response.read().decode("utf-8")
         headlines = json.loads(data)
         return make_response(jsonify(headlines), 200)
