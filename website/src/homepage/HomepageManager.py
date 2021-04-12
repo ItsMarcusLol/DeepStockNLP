46ee27c6b8e8d251543899a605364a77a86c2072
Marcus Lee <leemg@plu.edu>
2021-04-12 14:31:27 -0700

    Added API for homepage messages

--- website/src/homepage/HomepageManager.py
+++ website/src/homepage/HomepageManager.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify, make_response
 from urllib.request import urlopen
 import json
 import time
+import mysql.connector
+
+conn = mysql.connector.connect(user='root', password='MarLee21!', host='db', database='forum')
 
 class HomepageManager():
     def get_headlines(self, symbol):
@@ -11,3 +14,11 @@ class HomepageManager():
         data = response.read().decode("utf-8")
         headlines = json.loads(data)
         return make_response(jsonify(headlines), 200)
+    
+    def get_messages(self):
+        cursor = conn.cursor()
+        query = "Select * from forum_data"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return make_response(jsonify(), 200)
