9f2546a0ff013ecacf352ed7d29118104c5325b9
Nolan Premo <premonr@plu.edu>
2021-04-20 13:54:07 -0700

    Added basic error handling to HomepageManager

--- website/src/homepage/HomepageManager.py
+++ website/src/homepage/HomepageManager.py
@@ -8,27 +8,36 @@ conn = mysql.connector.connect(user='root', password='MarLee21!', host='db', dat
 
 class HomepageManager():
     def get_headlines(self, symbol):
-        key = "f0448bd30a7028e245052fcf3caa0837"
-        url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=" + symbol + "&limit=4&apikey=" + key
-        response = urlopen(url)
-        data = response.read().decode("utf-8")
-        headlines = json.loads(data)
-        return make_response(jsonify(headlines), 200)
+        try: 
+            key = "f0448bd30a7028e245052fcf3caa0837"
+            url = "https://financialmodelingprep.com/api/v3/stock_news?tickers=" + symbol + "&limit=4&apikey=" + key
+            response = urlopen(url)
+            data = response.read().decode("utf-8")
+            headlines = json.loads(data)
+            return make_response(jsonify(headlines), 200)
+        except: 
+            return make_response(jsonify({"message": "Server error"}), 500)
     
     def get_current(self):
-        key = "f0448bd30a7028e245052fcf3caa0837"
-        url = "https://financialmodelingprep.com/api/v3/quote/GOOGL,AAPL,BA,WMT,AMZN,TSLA,MSFT,F,DELL,TGT?apikey=" + key
-        response = urlopen(url)
-        data = response.read().decode("utf-8")
-        prices = json.loads(data)
-        return make_response(jsonify(prices), 200)
+        try: 
+            key = "f0448bd30a7028e245052fcf3caa0837"
+            url = "https://financialmodelingprep.com/api/v3/quote/GOOGL,AAPL,BA,WMT,AMZN,TSLA,MSFT,F,DELL,TGT?apikey=" + key
+            response = urlopen(url)
+            data = response.read().decode("utf-8")
+            prices = json.loads(data)
+            return make_response(jsonify(prices), 200)
+        except:
+            return make_response(jsonify({"message": "Server error"}), 500)
     
     def get_messages(self):
-        cursor = conn.cursor()
-        query = "Select * from forum_data"
-        cursor.execute(query)
-        result = cursor.fetchall()
-        cursor.close()
-        return make_response(jsonify(result), 200)
+        try: 
+            cursor = conn.cursor()
+            query = "Select * from forum_data"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            cursor.close()
+            return make_response(jsonify(result), 200)
+        except: 
+            return make_response(jsonify({"message": "Server error"}), 500)
 
     
