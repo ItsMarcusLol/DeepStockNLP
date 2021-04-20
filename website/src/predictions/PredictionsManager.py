21cfc0c2a3b5eef21729215c2bcd3470bdc0cbe6
Marcus Lee <leemg@plu.edu>
2021-04-20 13:12:40 -0700

    Created prediction service

---
+++ website/src/predictions/PredictionsManager.py
@@ -0,0 +1,29 @@
+import json
+import mysql.connector
+from flask import Flask, jsonify, make_response
+
+conn = mysql.connector.connect(user='root', password='MarLee21!', host='db', database='predictions')
+
+class PredictionsManager():
+    def get_predictions(self, ticker):
+        cursor = conn.cursor()
+        query = "SELECT * FROM prediction_data WHERE ticker=" + ticker
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return make_response(jsonify(result), 200)
+
+    def post_predictions(self, ticker, date, prediction, con, acc):
+        cursor = conn.cursor()
+        query = "INSERT INTO prediction_data(ticker,date,prediction,confidence) VALUES(\""+ticker+"\","+date+","+str(prediction)+","+str(con)+","+str(acc)+")"
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        return make_response(jsonify({"message": "Prediction Added"}), 200)
+    
+    def delete_predictions(self, ticker):
+        cursor = conn.cursor()
+        query = "DELETE FROM prediction_data WHERE ticker=" + ticker
+        cursor.execute(query)
+        cursor.close()
+        return make_response(204)
