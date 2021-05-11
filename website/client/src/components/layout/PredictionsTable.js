a0c819317d8dcfe63960a9c9ea2db55361841dd7
Vanessa <hoangvm@plu.edu>
2021-05-11 13:52:24 -0700

    Added description on prediction table, got rid of confidence on prediction table

--- website/client/src/components/layout/PredictionsTable.js
+++ website/client/src/components/layout/PredictionsTable.js
@@ -71,34 +71,22 @@ export default class PredictionsTable extends React.Component {
                 display: "flex",
                 fontSize: 33
               }}>
-                  Our Predictions
+                <pre style={{
+                  display: "flex", 
+                  fontFamily: "inherit",
+                  fontSize: 33}}>
+                  Our Predictions {'\n'}
+
+                  1 will go up, 0 will go down
+                </pre>
               </div>
         }  
-
-      // columns={[
-      //   { title: 'Date', field: 'date' },
-      //   { title: 'Time', field: 'time', type: 'numeric' },
-      //   { title: 'Prediction ', field: 'prediction', type: 'numeric' },
-      //   // { title: 'Low', field: 'low', type: 'numeric' },
-      //   // { title: 'Close', field: 'close', type: 'numeric' },
-      //   // { title: 'Volume', field: 'volume', type: 'numeric'}
-        
-      // ]}
-      
-      // data={[
-      //   { date: '3-24-2021', time: '1234', prediction: '1', high: '9393', low: '102', volume: '300' },
-      //   // { date: '3-23-2021', open: '1234', close: '2313', high: '5840', low: '302', volume: '300' },
-      //   // { date: '3-22-2021', open: '1234', close: '2313', high: '5943', low: '583', volume: '300' },
-      //   // { date: '3-21-2021', open: '1234', close: '2313', high: '8382', low: '452', volume: '300' },
-      //   // { date: '3-20-2021', open: '1234', close: '2313', high: '2048', low: '852', volume: '300' },
-       
-      // ]}
 
       columns={[
         { title: 'Ticker', field: 'ticker' },
         { title: 'Date', field: 'date'},
         { title: 'Prediction', field: 'prediction' },
-        { title: 'Confidence', field: 'con'},
+        // { title: 'Confidence', field: 'con'},
         { title: 'Accuracy', field: 'acc' }
       ]}
       
